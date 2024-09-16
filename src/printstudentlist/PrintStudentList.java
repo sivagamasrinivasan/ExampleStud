045f9103c724f96e8c227cc16f1bdf26545212a2
sivagamasrinivasan <784108@pdsb.net>
2024-09-16 18:06:43 -0400

    studentlist version 1

---
+++ src/printstudentlist/PrintStudentList.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package printstudentlist;
+
+/**
+ *
+ * @author sivagamasrinivasan
+ */
+public class PrintStudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // print 3 students name,sid using array of object
+        Student s1 = new Student();
+        s1.setName("peter");
+        s1.setId(1);
+        Student s2 = new Student();
+        s2.setName("john");
+        s2.setId(2);
+        Student s3 = new Student();
+        s3.setName("james");
+        s3.setId(3);
+        //s1 - name , sid - not in array - save  object
+        Student[] list = new Student[3]; //  array of objects
+        list[0]=s1;
+        list[1]=s2;
+        list[2]=s3; //saved the objects in array
+        //print 
+        for(int i=0;i<list.length;i++)
+        {
+          System.out.println(list[i].getName()+ " "+list[i].getId());  
+        }
+    }
+    
+}
---
+++ src/printstudentlist/Student.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package printstudentlist;
+
+/**
+ *
+ * @author sivagamasrinivasan
+ */
+public class Student 
+{
+    //data member - raw data for studnet entity
+    // model the student details
+    private String name; // public or private
+    private int id; // Encapsulation principle 
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+}
